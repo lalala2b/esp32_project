d6a2ad67636c00367b65b4740fb20c3b317dc79e
lalala <55229767+lalala2b@users.noreply.github.com>
2023-11-03 15:10:15 +0800

    Add files via upload

---
+++ ESP NOW/ESP_NOW_TWO_WAY/ESP_NOW_TWO_WAY.ino
@@ -0,0 +1,112 @@
+// BOX PUTIH
+
+#include <esp_now.h>
+#include <WiFi.h>
+#include <DHTesp.h>
+#include <Wire.h>
+#include <LiquidCrystal.h>
+
+
+//menggunakan lcd
+LiquidCrystal lcd(2,4,15,5,18,19);
+#define DHT_PIN 23
+DHTesp dht;
+
+float temperature;
+float humidity;
+
+float incomingTemp;
+float incomingHum;
+
+String success;
+// gunakan address esp32
+uint8_t broadcastAddress[] = {0x94,0xB9,0x7E,0xC4,0xDE,0xD4};
+
+typedef struct struct_message{
+  float temp;
+  float hum;
+}struct_message;
+
+struct_message DHTread;
+
+struct_message incomingMessage;
+
+esp_now_peer_info_t peerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status){
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if(status==0){
+    success = "Delivery Success :)";
+  }else{
+    success = "Delivery Fail :(";
+  }
+}
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData,int len){
+  memcpy(&incomingMessage, incomingData, sizeof(incomingMessage));
+  Serial.println("Bytes received: "+String(len));
+  incomingTemp = incomingMessage.temp;
+  incomingHum = incomingMessage.hum;
+}
+
+void setup(){
+  Serial.begin(115200);
+  lcd.begin(20,4);
+  WiFi.mode(WIFI_STA);
+  dht.setup(DHT_PIN,DHTesp::DHT11);
+  
+  if(esp_now_init() != ESP_OK){
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(OnDataSent);
+
+  memcpy(peerInfo.peer_addr,broadcastAddress,6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void loop(){
+  getReading();
+
+  DHTread.temp = temperature;
+  DHTread.hum = humidity;
+
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &DHTread, sizeof(DHTread));
+
+  if (result == ESP_OK){
+    Serial.println("Sent with success");
+  }else{
+    Serial.println("Error sending the data");
+  }
+  updateDisplay();
+  delay(1000);
+}
+
+void getReading(){
+  TempAndHumidity data = dht.getTempAndHumidity();
+
+  temperature = data.temperature;
+  humidity = data.humidity;
+}
+
+void updateDisplay(){
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print("Data Diterima");
+  lcd.setCursor(0,2);
+  lcd.print("Temp: "+String(incomingTemp)+"C");
+  lcd.setCursor(0,3);
+  lcd.print("Hum: "+String(incomingHum)+"%");
+  Serial.println("Temp: "+String(incomingTemp)+"C");
+  Serial.println("Hum: "+String(incomingHum)+"%");
+}
