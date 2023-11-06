9231dc812c0cd2cb4d5d888b47d889452e1a7119
lalala <55229767+lalala2b@users.noreply.github.com>
2023-11-06 12:48:23 +0800

    Add files via upload

---
+++ ESP NOW/New_test.ino
@@ -0,0 +1,138 @@
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Arduino.h>
+
+#include <DHTesp.h>
+DHTesp dhtsensor;
+TempAndHumidity data;
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+#include <Wire.h>
+
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+// Define variables to store BME280 readings to be sent
+float temperature;
+float humidity;
+
+// Define variables to store incoming readings
+float incomingTemp;
+float incomingHum;
+
+// Variable to store if sending data was successful
+String success;
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct struct_message {
+    float temp;
+    float hum;
+} struct_message;
+
+// Create a struct_message called DHTReadings to hold sensor readings
+struct_message DHTReadings;
+
+// Create a struct_message to hold incoming sensor readings
+struct_message incomingReadings;
+
+esp_now_peer_info_t peerInfo;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    success = "Delivery Success :)";
+  }
+  else{
+    success = "Delivery Fail :(";
+  }
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Temperature from other ESP:");
+  Serial.println(incomingReadings.temp);
+  Serial.print("Humidity from other ESP:");
+  Serial.println(incomingReadings.hum);
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+	dhtsensor.setup(DHTPIN, DHTTYPE);
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+  getReadings();
+ 
+  // Set values to send
+  DHTReadings.temp = temperature;
+  DHTReadings.hum = humidity;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &DHTReadings, sizeof(DHTReadings));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  updateDisplay();
+  delay(10000);
+}
+void getReadings(){
+  // temperature = bme.readTemperature();
+  // humidity = bme.readHumidity();
+  // bagian yang kuubah
+  data=dhtsensor.getTempAndHumidity();
+  temperature = data.temperature;
+  humidity = data.humidity;
+  delay(1000);
+}
+
+void updateDisplay(){
+  
+  // Display Readings in Serial Monitor
+  Serial.println();
+  Serial.println("INCOMING READINGS");
+  Serial.print("Temperature: ");
+  Serial.print(incomingReadings.temp);
+  Serial.println(" ºC");
+  Serial.print("Humidity: ");
+  Serial.print(incomingReadings.hum);
+  Serial.println(" %");
+  Serial.println();
+
+}
